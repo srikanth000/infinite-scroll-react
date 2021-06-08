93899527dde7e5ad7160234200a757daef43f897
srikanth000 <margamsrikanth7@gmail.com>
2021-06-08 09:58:06 +0530

    add: cards and css

---
+++ src/components/organisms/DisplayCards.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const DisplayCards = ({listData, loading}) => {
+  return (
+    <div className='container'>
+      <h2 className='text'>Posts</h2>
+      {listData.map((item, index) => {
+        return (
+          <div className={'card'}>
+            <h3>{item.title}</h3>
+            <p>{item.body}</p>
+          </div>
+        )
+      })}
+      {loading && <h3>fetching data.....</h3>}
+    </div>   
+  )
+}
+
+export default DisplayCards;
